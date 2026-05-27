7372b0fcdb7c8f2ce5019881aec09d26b7558c3f
Luka Kartvelishvili <lukaqartveli112@gmail.com>
2026-05-27 14:50:37 +0400

    fix dependencies and update project

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/My-Portfolio/" : "/",
+  base: process.env.VERCEL ? "/" : "/My-Portfolio/",
   plugins: [
     react()
   ],
